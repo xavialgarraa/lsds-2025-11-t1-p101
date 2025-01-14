37ea54b15292ce1fc8145b33eeb15e18d2fbcca9
xavialgarraa <javier.algarra01@estudiant.upf.edu>
2025-01-14 11:32:30 +0100

    api sum upload

--- projects/1-hdfs/testserver1/main.py
+++ projects/1-hdfs/testserver1/main.py
@@ -1,15 +1,23 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Define the model for the request body
+class SumRequest(BaseModel):
+    x: float
+    y: float
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/sum")
+def calculate_sum(payload: SumRequest):
+    result = payload.x + payload.y
+    return {"result": result}
