167df4b86a6d1841d2d6154c5b4e70ad7061d6e7
lucassandreu <lucas.andreu01@estudiant.upf.edu>
2025-01-15 13:36:43 +0100

    docker in server1

--- projects/1-hdfs/testserver1/app/main.py
+++ projects/1-hdfs/testserver1/app/main.py
@@ -4,18 +4,22 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 # Define the model for the request body
 class SumRequest(BaseModel):
     x: float
     y: float
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
 
 @app.post("/sum")
 def calculate_sum(payload: SumRequest):
