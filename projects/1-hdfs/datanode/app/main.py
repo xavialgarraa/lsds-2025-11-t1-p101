ffd8a38f38c27d709e30a4335f2ff0f6493078b9
xavialgarraa <javier.algarra01@estudiant.upf.edu>
2025-01-20 13:45:10 +0100

    GET L2Q1

--- projects/1-hdfs/datanode/app/main.py
+++ projects/1-hdfs/datanode/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
 import os
 
 app = FastAPI()
@@ -21,3 +23,20 @@ async def upload_file_block(filename: str, block_number: int, file: UploadFile):
     return {
         "message": f"Block {block_number} of file {filename} uploaded successfully."
     }
+
+
+@app.get("/files/{filename}/blocks/{block_number}/content")
+async def get_file_block(filename: str, block_number: int):
+    # Path to the block
+    storage_path = os.path.join("datanode/storage", filename, str(block_number))
+    # Check if the file exists
+    if not os.path.exists(storage_path):
+        raise HTTPException(status_code=404, detail="Block not found")
+    
+    print(f"Checking file at: {storage_path}")
+
+    # Return the file
+    try:
+        return FileResponse(storage_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading file block: {e}")
