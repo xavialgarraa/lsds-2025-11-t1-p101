90a92bd6a9bfacd464b699efc6347b3ee1c030ff
xavialgarraa <javier.algarra01@estudiant.upf.edu>
2025-01-22 13:56:10 +0100

    Correct namenode

--- projects/1-hdfs/namenode/app/main.py
+++ projects/1-hdfs/namenode/app/main.py
@@ -22,23 +22,24 @@ def save_files(data):
         current_data = {}
         with open("app/files.json", "r") as file:
             current_data = json.load(file)
-        # Append the new data to the current data dict
-        if data["file_name"] not in current_data:
-            current_data[data["file_name"]] = data
-        else:
-            raise HTTPException(status_code=409, detail=f"File already in dictoionary.")
-
-        # Save the updated data back to the file
-        try:
-            with open("app/files.json", "w") as file:
-                json.dump(current_data, file, indent=4)
-            print("Data saved successfully.")
-        except Exception as e:
-            print(f"Error writing to JSON file: {e}")
-            raise RuntimeError("Failed to save data to the JSON file.")
     except Exception as e:
-        print(f"Unexpected error in save_files: {e}")
-        raise RuntimeError("Failed to save the file in the system.")
+            print(f"Unexpected error in read the file: {e}")
+            raise RuntimeError("Failed to read the file in the system.")
+
+    # Append the new data to the current data dict
+    if data["file_name"] not in current_data:
+        current_data[data["file_name"]] = data
+    else:
+        raise HTTPException(status_code=409, detail="File already in dictionary.")
+
+    # Save the updated data back to the file
+    try:
+        with open("app/files.json", "w") as file:
+            json.dump(current_data, file, indent=4)
+        print("Data saved successfully.")
+    except Exception as e:
+        print(f"Error writing to JSON file: {e}")
+        raise RuntimeError("Failed to save data to the JSON file.")
 
 
 def load_files():
@@ -76,54 +77,52 @@ def get_datanodes():
 
 @app.post("/files")
 def upload_files(file: File):
-    try:
-        # Calculate the number of blocks needed
-        if file.size % block_size != 0:
-            num_blocks = (file.size // block_size) + 1
-        else:
-            num_blocks = file.size // block_size
+    # Calculate the number of blocks needed
+    if file.size % block_size != 0:
+        num_blocks = (file.size // block_size) + 1
+    else:
+        num_blocks = file.size // block_size
 
-        blocks = []  # List to store block information
-        rest_size = file.size
+    blocks = []  # List to store block information
+    rest_size = file.size
 
-        # Create blocks with size and replicas
-        for i in range(num_blocks):
-            size = min(block_size, rest_size)
-            rest_size -= size
-            datanode_idx = i % len(datanodes)
+    # Create blocks with size and replicas
+    for i in range(num_blocks):
+        size = min(block_size, rest_size)
+        rest_size -= size
+        datanode_idx = i % len(datanodes)
 
-            replicas = []
-            for j in range(num_replicas):
-                replica_idx = (datanode_idx + j) % len(datanodes)
-                replicas.append(datanodes[replica_idx])
-            blocks.append(
-                {
-                    "number": i,
-                    "size": size,
-                    "replicas": replicas,
-                }
-            )
+        replicas = []
+        for j in range(num_replicas):
+            replica_idx = (datanode_idx + j) % len(datanodes)
+            replicas.append(datanodes[replica_idx])
+        blocks.append(
+            {
+                "number": i,
+                "size": size,
+                "replicas": replicas,
+            }
+        )
 
-        data = {"file_name": file.name, "size": file.size, "blocks": blocks}
-        save_files(data)  # Save the file data
+    data = {"file_name": file.name, "size": file.size, "blocks": blocks}
+    save_files(data)  # Save the file data
 
-        return {
-            "file_name": file.name,
-            "size": file.size,
-            "number_blocks": num_blocks,
-            "blocks": blocks,
-        }
-
-    except Exception as e:
-        print(f"Unexpected error in upload_files: {e}")
-        return {"error": "An unexpected error occurred while processing the file."}
+    return {
+        "file_name": file.name,
+        "size": file.size,
+        "number_blocks": num_blocks,
+        "blocks": blocks,
+    }
 
 
 @app.get("/files/{filename}")
 def read_file(filename: str):
     files_metadata = load_files()
-    for file_info in files_metadata:
-        if file_info["file_name"] == filename:
-            return file_info
 
-    raise HTTPException(status_code=404, detail=f"File {filename} not found.")
+    if filename in files_metadata:
+        return files_metadata[filename]
+    else:
+        raise HTTPException(status_code=400, detail=f"File {filename} not found.")
+
+
+
