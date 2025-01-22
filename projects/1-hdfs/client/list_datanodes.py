276d515bb9ce4edc11f35d37dbd09565f47ddf76
lucassandreu <lucas.andreu01@estudiant.upf.edu>
2025-01-22 14:19:58 +0100

    Create list_datanodes.py

---
+++ projects/1-hdfs/client/list_datanodes.py
@@ -0,0 +1,16 @@
+import requests
+
+NN_URL = "http://localhost:8000/datanodes"
+
+
+def list_datanodes():
+    # Make a GET request to the namenode API
+    r = requests.get(NN_URL)
+    datanodes = r.json()
+    print("List of Datanodes:")
+    for idx, datanode in enumerate(datanodes, start=1):
+        print(f"{idx}. Host: {datanode['host']}, Port: {datanode['port']}")
+
+
+if __name__ == "__main__":
+    list_datanodes()
