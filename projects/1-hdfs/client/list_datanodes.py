7c385613342ea243cf8bfc0efd888a0a163d11c4
jordilleopart <jordi.lleopart01@estudiant.upf.edu>
2025-01-30 11:42:56 +0100

    S2Q0 fixed
    def added as suposed to be + if

--- projects/1-hdfs/client/list_datanodes.py
+++ projects/1-hdfs/client/list_datanodes.py
@@ -2,9 +2,15 @@ import requests
 
 NN_URL = "http://localhost:8000/datanodes"
 
-r = requests.get(NN_URL)
-datanodes = r.json()
-print("List of Datanodes:")
 
-for idx, datanode in enumerate(datanodes["datanodes"]):
-    print(f"Datanode: {idx + 1}, Port: {datanode['port']}")
+def list_datanodes():
+    r = requests.get(NN_URL)
+    datanodes = r.json()
+    print("List of Datanodes:")
+
+    for idx, datanode in enumerate(datanodes["datanodes"]):
+        print(f"Datanode: {idx + 1}, Port: {datanode['port']}")
+
+
+if __name__ == "__main__":
+    list_datanodes()
