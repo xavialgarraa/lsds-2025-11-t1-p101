077ef374072830e365f999481c2e25a73dd146d6
jordilleopart <jordi.lleopart01@estudiant.upf.edu>
2025-02-02 19:25:13 +0100

    Delete delete.py and list_files_in_namenode.py
    Deleted 2 .py files

--- projects/1-hdfs/client/download.py
+++ projects/1-hdfs/client/download.py
@@ -20,9 +20,7 @@ def download_file_blocks(filename, metadata, output_path):
     with open(output_path, "wb") as destination_file:
         for block_info in metadata["blocks"]:
             block_id = block_info["number"]
-            primary_replica = block_info["replicas"][
-                0
-            ]  # Download from the first available replica
+            primary_replica = block_info["replicas"][0]
             block_url = f"http://{primary_replica['host']}:{primary_replica['port']}/files/{filename}/blocks/{block_id}/content"
 
             print(f"Downloading block {block_id} from {block_url}...")
@@ -38,13 +36,13 @@ def download_file_blocks(filename, metadata, output_path):
 
 
 if __name__ == "__main__":
-    filename = input("Enter the name of the file to download: ").strip()
+    fname = input("Enter the name of the file to download: ").strip()
     output_path = input("Enter the destination path: ").strip()
 
-    metadata = fetch_file_metadata(filename)
+    metadata = fetch_file_metadata(fname)
     if metadata:
-        download_successful = download_file_blocks(filename, metadata, output_path)
-        if download_successful:
-            print(f"File '{filename}' successfully downloaded to '{output_path}'")
+        download_success = download_file_blocks(fname, metadata, output_path)
+        if download_success:
+            print(f"File '{fname}' successfully downloaded to '{output_path}'")
         else:
             print(f"An error occurred while downloading the file.")
--- projects/1-hdfs/client/upload.py
+++ projects/1-hdfs/client/upload.py
@@ -5,22 +5,23 @@ import os
 NAMENODE_URL = "http://localhost:8000"
 
 
-def get_file_info():
+def get_upload_file_info():
     """Prompts the user for the file path and retrieves its size."""
-    file_path = input("Enter the file path: ").strip()
-    if not os.path.exists(file_path):
+    upload_fpath = input("Enter the file path: ").strip()
+    if not os.path.exists(upload_fpath):
         print("Error: The file does not exist.")
         return None, None, None
 
-    file_name = input("Enter the file name in the HDFS: ").strip()
-    file_size = os.path.getsize(file_path)
-    return file_path, file_name, file_size
+    upload_fname = input("Enter the file name in the HDFS: ").strip()
+    upload_fsize = os.path.getsize(upload_fpath)
+    return upload_fpath, upload_fname, upload_fsize
 
 
-def create_file_in_namenode(file_name, file_size):
+def create_upload_file_in_namenode(upload_fname, upload_fsize):
     """Creates a file in the namenode and gets the block allocation."""
     response = requests.post(
-        f"{NAMENODE_URL}/files", json={"name": file_name, "size": file_size}
+        f"{NAMENODE_URL}/files",
+        json={"name": upload_fname, "size": upload_fsize},
     )
     if response.status_code == 200:
         return response.json()
@@ -29,20 +30,15 @@ def create_file_in_namenode(file_name, file_size):
         return None
 
 
-def upload_blocks(file_path, file_name, file_metadata):
+def upload_file_blocks(upload_fpath, upload_fname, upload_fmetadata):
     """Reads the file in blocks and uploads them to the assigned datanodes."""
-    with open(file_path, "rb") as f:
-        for block in file_metadata["blocks"]:
-            block_data = f.read(block["size"])  # Read the block size
-
+    with open(upload_fpath, "rb") as f:
+        for block in upload_fmetadata["blocks"]:
+            block_data = f.read(block["size"])
             for replica in block["replicas"]:
-                datanode_url = f"http://{replica['host']}:{replica['port']}/files/{file_name}/blocks/{block['number']}/content"
-
-                # Debugging
+                datanode_url = f"http://{replica['host']}:{replica['port']}/files/{upload_fname}/blocks/{block['number']}/content"
                 print(f"Uploading block {block['number']} to {datanode_url}")
-
                 response = requests.put(datanode_url, files={"file": block_data})
-
                 if response.status_code == 200:
                     print(
                         f"Block {block['number']} successfully uploaded to {datanode_url}"
@@ -54,9 +50,9 @@ def upload_blocks(file_path, file_name, file_metadata):
 
 
 if __name__ == "__main__":
-    local_path, hdfs_name, size = get_file_info()
+    upload_path, hdfs_upload_name, upload_size = get_upload_file_info()
 
-    if local_path and hdfs_name and size:
-        metadata = create_file_in_namenode(hdfs_name, size)
-        if metadata:
-            upload_blocks(local_path, hdfs_name, metadata)
+    if upload_path and hdfs_upload_name and upload_size:
+        upload_metadata = create_upload_file_in_namenode(hdfs_upload_name, upload_size)
+        if upload_metadata:
+            upload_file_blocks(upload_path, hdfs_upload_name, upload_metadata)
