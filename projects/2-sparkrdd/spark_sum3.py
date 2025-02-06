6f2786c2653a72ba911b103b70ba9de1ab5fc81d
lucassandreu <131767021+lucassandreu@users.noreply.github.com>
2025-02-06 11:39:23 +0100

    S3Q2 - spark_sum3 done

--- projects/2-sparkrdd/spark_sum3.py
+++ projects/2-sparkrdd/spark_sum3.py
@@ -1,1 +1,27 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql.functions import *
+import sys
 
+_, source = sys.argv
+
+conf = SparkConf().setAppName("spark-sum2")
+sc = SparkContext(conf=conf)
+
+numbers_rdd = sc.textFile(source)
+numbers_int_rdd = numbers_rdd.flatMap(lambda line: map(int, line.split()))
+even_numbers_rdd = numbers_int_rdd.filter(lambda x: x % 2 == 0)
+result = even_numbers_rdd.sum()
+
+print(
+    f"""
+------------------------------------------
+------------------------------------------
+------------------------------------------
+SUM = {result}
+------------------------------------------
+------------------------------------------
+------------------------------------------
+"""
+)
+
+sc.stop()
